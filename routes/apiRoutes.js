c082f5ff7a744f4d5d0b16af81cbaadce4ea1db2
Marwin Manlangit <marwin.m@hotmail.com>
2023-02-09 01:20:23 +1100

    Pushing everything thus far to test heroku

---
+++ routes/apiRoutes.js
@@ -0,0 +1,25 @@
+const router = require('express').Router();
+const dbstore = require('../db/dbstore');
+
+router.get('/notes', function (req, res) {
+    dbstore
+        .retrieveNotes()
+        .then(notes => res.json(notes))
+        .catch(err => res.status(500).json(err));
+});
+
+router.post('/notes', (req, res) => {
+    dbstore
+        .addNote(req.body)
+        .then((note) => res.json(note))
+        .catch(err => res.status(500).json(err));
+});
+
+router.delete('/notes/:id', function (req, res) {
+    dbstore
+        .deleteNote(req.params.id)
+        .then(() => res.json({ ok: true }))
+        .catch(err => res.status(500).json(err));
+});
+
+module.exports = router;
